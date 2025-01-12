dfd071c39f1a3c0d7de2a8fa992920c391700230
ChitChime <chitchime@gmail.com>
2025-01-12 04:24:19 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ["example.com", "cdn.example.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NODE_ENV === "development",
+  },
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === "development",
+  },
+};
+
+export default nextConfig;
